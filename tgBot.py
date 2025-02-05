ec5bf7accb4fa0e105f8d673d58782a69c29dc56
JustDev <savelijln0@gmail.com>
2025-02-05 15:15:27 +0400

    Some updates

--- tgBot.py
+++ tgBot.py
@@ -622,7 +622,7 @@ def isFirstApril():
     return datetime.today().month == 4 and datetime.today().day == 1
 
 def ThreePercentChance():
-    return random.randint(1, 100) <= 5
+    return random.randint(1, 100) <= 3
 
 
 @bot.message_handler(commands=['gmt'])
