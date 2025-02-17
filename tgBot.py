e72b0e8d54ba88a32bd0ca1b9a8a46d26bb459d3
JustDev <savelijln0@gmail.com>
2025-02-17 21:13:45 +0400

    Some correction in code

--- tgBot.py
+++ tgBot.py
@@ -770,7 +770,7 @@ def DynamicMessage(message):
             DBMessages.UnRegisterMessageReloader(DynamicChatID)
         except: print("Failed to delete dynamic message")
 
-    Message = bot.send_message(uid, text=ShedForDay, message_thread_id=forum, reply_markup=get_keyboard(), parse_mode="MarkdownV2")
+    Message = bot.send_message(uid, text=ShedForDay, message_thread_id=forum, reply_markup=get_keyboard(), parse_mode="MarkdownV2", disable_notification=True)
     DynamicChatMessage, botin['DynamicID'] = 2 * [Message.message_id]
     DynamicChatID, botin['DynamicChatID'] = 2 * [uid]
     DynamicForumID, botin['DynamicForumID'] = 2 * [forum]
