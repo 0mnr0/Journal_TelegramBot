233610d516b4a1a0a74c008f9826a2ac6d3c90d6
JustDev <savelijln0@gmail.com>
2025-04-01 09:57:01 +0400

    Fix easter egg on first april

--- tgBot.py
+++ tgBot.py
@@ -556,7 +556,7 @@ def fetchDate(message, Relaunch=False, Sended=None):
     uid = str(message.chat.id)
     forum = isForum(message)
 
-    if isFirstApril():
+    if isFirstApril() and not Relaunch:
         with open("EasterEggs/shedule_in_4k.jpg", "rb") as photo:
             bot.send_photo(
                 chat_id=message.chat.id,
